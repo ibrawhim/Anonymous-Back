c36d51bf6ef6abc4eea5b01188dd0533ffef5523
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-10-02 12:39:57 +0100

    anonymous-back

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const userRouter = require('./routes/user.route')
 const cors = require('cors')
 
 
-app.use(express.urlencoded({extended:true, limit:'50mb'}))
 app.use(cors())
+app.use(express.urlencoded({extended:true, limit:'50mb'}))
 app.use(express.json({limit:'50mb'}))
 app.use('/', userRouter)
 
