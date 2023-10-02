485855dc2b641c89bb437816097bfc62f5a78259
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-10-02 12:35:44 +0100

    anonymous-back

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const mongoose = require('./connections/mongoose.connection')
 const app = express()
 const userRouter = require('./routes/user.route')
 const cors = require('cors')
 
 
-app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}))
+app.use(express.urlencoded({extended:true, limit:'50mb'}))
 app.use(cors())
-app.use(bodyParser.json({limit:'50mb'}))
+app.use(express.json({limit:'50mb'}))
 app.use('/', userRouter)
 
 
