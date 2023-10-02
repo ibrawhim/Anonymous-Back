bc797851f8b1ac4a7a5b5467f660e149f010f0b3
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-10-02 12:31:41 +0100

    anonymous-back

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}))
 app.use(cors())
 app.use(bodyParser.json({limit:'50mb'}))
-app.use('/user', userRouter)
+app.use('/', userRouter)
 
 
 
