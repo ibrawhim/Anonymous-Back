52a51328c2220390cb2602f8af66658dccb2acd8
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-10-03 01:35:29 +0100

    anonymous

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const bodyParser = require('body-parser')
-const mongoose = require('./connections/mongoose.connection')
-const userRouter = require('./routes/user.route')
-
-
 app.use(cors())
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const userRouter = require('./routes/user.route')
+const dotenv = require('dotenv')
+dotenv.config()
+
 app.use(express.urlencoded({extended:true,limit:'50mb'}))
 app.use(bodyParser.urlencoded({extended: true,limit:'50mb'}))
 app.use(express.json({limit:'50mb'}))
 app.use('/', userRouter)
 
+let URI = process.env.MONGO
+
+mongoose.connect(URI)
+.then(()=>{
+    console.log('mongoose connected');
+})
+.catch((err)=>{
+    console.log(err);
+})
 
 
 app.listen('4678',()=>{
